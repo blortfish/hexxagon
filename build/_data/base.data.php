5a843fd019bed1f6b6c603aceab0759e186f123b
Daniel Flint <def6054@rit.edu>
2016-04-15 12:19:27 -0400

    added mysql connection error message

--- build/_data/base.data.php
+++ build/_data/base.data.php
@@ -6,8 +6,12 @@
 
         function __construct()
         {
-            include("../../db.cred.inc");
-            $this->connection = new mysqli("thoughtfulbadger.com", $user, $password, $db);
+            include("../db.cred.inc");
+            $this->connection = @new mysqli("127.0.0.1", $user, $password, $db);
+            if($this->connection->connect_error) {
+                echo "Error connecting to database, please check settings in {projectRoot}/db.cred.inc and make sure " .
+                    "that mysql is running";
+            }
         }
 
         function getConn()
